b4d885a766edca806884d85ede49c594375c12d5
Stefan Taubert <stefan.taubert@posteo.de>
2021-03-24 12:34:40 +0100

    added more to init

--- image_utils/__init__.py
+++ image_utils/__init__.py
@@ -1,4 +1,6 @@
 from image_utils.main import (calculate_structual_similarity,
                               calculate_structual_similarity_np,
+                              make_same_width_by_cutting_larger_img,
+                              make_same_width_by_filling_white,
                               stack_images_horizontally,
                               stack_images_vertically)
