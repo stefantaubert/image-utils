94ab7af44b863b4562c352dc8d354606d005d7cc
Stefan Taubert <stefan.taubert@posteo.de>
2021-06-01 10:08:32 +0200

    .

--- setup.py
+++ setup.py
@@ -1,19 +1,4 @@
-from setuptools import find_packages, setup
+import setuptools
 
-setup(
-    dependency_links=[],
-    name="image_utils",
-    version="1.0.0",
-    url="https://github.com/stefantaubert/image-utils.git",
-    author="Stefan Taubert",
-    author_email="stefan.taubert@posteo.de",
-    description="Utils for image processing",
-    packages=["image_utils"],
-    install_requires=[
-        "imageio",
-        "matplotlib",
-        "numpy",
-        "pillow",
-        "scikit-image",
-    ],
-)
+if __name__ == "__main__":
+  setuptools.setup()
